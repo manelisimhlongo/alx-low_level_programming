8492bc9e61d9f5eab511eb591cc977aa4e17fbee
Manelisi Mhlongo <manelisi@atlasdot.co.za>
2023-06-26 08:00:31 -0700

    gfile 9 changed

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,12 +11,10 @@
  * Return: pointer to dest
  */
 char* _strcpy(char* dest, char* src) {
-    char* original_dest = dest;
+    char *original_dest = dest;
 
     while (*src != '\0') {
-        *dest = *src;
-        dest++;
-        src++;
+        *dest++ = *src++;
     }
 
     *dest = '\0'; 
